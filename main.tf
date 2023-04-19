25959204d8385d07fec4be79f2c3f102c354ffdb
iraj norouzi <i.norouzi@mail.cloud-infra.ir>
2023-04-19 14:11:02 +0330

    asd

--- main.tf
+++ main.tf
@@ -1,24 +1,17 @@
-# Terraform Block
 terraform {
-  required_version = "~> 0.14.6"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 4.0"
     }
   }
 }
 
-# Provider Block
+# Configure the AWS Provider
 provider "aws" {
-  region  = "us-east-1"
-  profile = "default"
+  region = "us-east-1"
 }
 
-/*
-Note-1:  AWS Credentials Profile (profile = "default") configured on your local desktop terminal  
-$HOME/.aws/credentials
-*/
 
 resource "aws_instance" "foo" {
   ami           = "ami-069aabeee6f53e7bf" # us-west-2
